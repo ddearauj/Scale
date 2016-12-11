dd8f0cd91c4b0252242084aa90fed6103c7576ae
Daniel Pereira <danielpereira03@gmail.com>
2016-12-11 16:48:00 -0200

    added imports

--- MyPersonalScale/app/src/main/java/com/example/fernando/mypersonalscale/MainActivity.java
+++ MyPersonalScale/app/src/main/java/com/example/fernando/mypersonalscale/MainActivity.java
@@ -1,9 +1,40 @@
 package com.example.fernando.mypersonalscale;
 
 import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.os.Handler;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
+
+	private final String DEVICE_ADDRESS="20:13:10:15:33:66";
+    private final UUID PORT_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");//Serial Port Service ID
+    private BluetoothDevice device;
+    private BluetoothSocket socket;
+    private OutputStream outputStream;
+    private InputStream inputStream;
+    Button startButton, sendButton,clearButton,stopButton;
+    TextView textView;
+    EditText editText;
+    boolean deviceConnected=false;
+    Thread thread;
+    byte buffer[];
+    int bufferPosition;
+    boolean stopThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
