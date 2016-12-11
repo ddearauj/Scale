cdf1347a8e4e2f3431f88519f69e0a74e6212465
Daniel Pereira <danielpereira03@gmail.com>
2016-12-11 16:40:21 -0200

    bluetooth connection

--- MyPersonalScale/app/src/main/java/com/example/fernando/mypersonalscale/MainActivity.java
+++ MyPersonalScale/app/src/main/java/com/example/fernando/mypersonalscale/MainActivity.java
@@ -10,4 +10,85 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+	public boolean BTinit()
+	    {
+	        boolean found=false;
+	        BluetoothAdapter bluetoothAdapter=BluetoothAdapter.getDefaultAdapter();
+	        if (bluetoothAdapter == null) {
+	            Toast.makeText(getApplicationContext(),"Device doesnt Support Bluetooth",Toast.LENGTH_SHORT).show();
+	        }
+	        if(!bluetoothAdapter.isEnabled())
+	        {
+	            Intent enableAdapter = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+	            startActivityForResult(enableAdapter, 0);
+	            try {
+	                Thread.sleep(1000);
+	            } catch (InterruptedException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	        Set<BluetoothDevice> bondedDevices = bluetoothAdapter.getBondedDevices();
+	        if(bondedDevices.isEmpty())
+	        {
+	            Toast.makeText(getApplicationContext(),"Please Pair the Device first",Toast.LENGTH_SHORT).show();
+	        }
+	        else
+	        {
+	            for (BluetoothDevice iterator : bondedDevices)
+	            {
+	                if(iterator.getAddress().equals(DEVICE_ADDRESS))
+	                {
+	                    device=iterator;
+	                    found=true;
+	                    break;
+	                }
+	            }
+	        }
+	        return found;
+	    }
+
+    public boolean BTconnect()
+    {
+        boolean connected=true;
+        try {
+            socket = device.createRfcommSocketToServiceRecord(PORT_UUID);
+            socket.connect();
+        } catch (IOException e) {
+            e.printStackTrace();
+            connected=false;
+        }
+        if(connected)
+        {
+            try {
+                outputStream=socket.getOutputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                inputStream=socket.getInputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        return connected;
+    }
+
+
+    
+    public void onClickStart(View view) {
+        if(BTinit())
+        {
+            if(BTconnect())
+            {
+                setUiEnabled(true);
+                deviceConnected=true;
+                beginListenForData();
+                textView.append("\nConnection Opened!\n");
+            }
+
+        }
+    }
+
 }
