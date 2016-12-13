853c1c365d422e918afac2d8ef6c74df6bd0a1d0
Daniel Pereira <danielpereira03@gmail.com>
2016-12-13 04:40:44 -0200

    added bluetooth connectivity

--- app/src/main/java/com/example/dpereira14/scale/MainActivity.java
+++ app/src/main/java/com/example/dpereira14/scale/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.dpereira14.scale;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -25,8 +31,12 @@ import com.google.android.gms.fitness.data.DataType;
 import com.google.android.gms.fitness.data.Field;
 import com.google.android.gms.fitness.request.DataUpdateRequest;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 
@@ -37,7 +47,16 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     private static boolean connected = false;
     private GoogleApiClient mGoogleApiClient;
     TextView textView;
-    private static float weight = 50;
+    Button connectButton;
+    private static float weight = 98;
+    private final String DEVICE_NAME = "BT-820";
+    private BluetoothDevice device;
+    private BluetoothSocket socket;
+    private OutputStream outputStream;
+    private InputStream inputStream;
+    boolean deviceConnected = false;
+    byte buffer[];
+    boolean stopThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +65,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         textView = (TextView) findViewById(R.id.textView);
         textView.setText("Login to continue");
         findViewById(R.id.sign_in_button).setOnClickListener(this);
+        connectButton = (Button) findViewById(R.id.connectButton);
 
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -255,6 +275,107 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         }
         else {
             textView.setText("Login to send weight");
+        }
+    }
+
+
+
+    // BLUTOOTH
+
+    public boolean BTinit() {
+        boolean found = false;
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            Toast.makeText(getApplicationContext(), "Device doesnt Support Bluetooth", Toast.LENGTH_SHORT).show();
+        }
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableAdapter = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableAdapter, 0);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        Set<BluetoothDevice> bondedDevices = bluetoothAdapter.getBondedDevices();
+        if (bondedDevices.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please Pair the Device first", Toast.LENGTH_SHORT).show();
+        } else {
+            for (BluetoothDevice iterator : bondedDevices) {
+                if (iterator.getName().equals(DEVICE_NAME)) {
+                    device = iterator;
+                    found = true;
+                    break;
+                }
+            }
+        }
+        return found;
+    }
+
+    public boolean BTconnect() {
+        boolean connected = true;
+        try {
+            socket = (BluetoothSocket) device.getClass().getMethod("createRfcommSocket", new Class[]{int.class}).invoke(device, 1);
+            socket.connect();
+        } catch (Exception e) {
+            e.printStackTrace();
+            connected = false;
+        }
+        if (connected) {
+            try {
+                outputStream = socket.getOutputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                inputStream = socket.getInputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        return connected;
+    }
+
+    void beginListenForData() {
+        final Handler handler = new Handler();
+        stopThread = false;
+        buffer = new byte[1024];
+        Thread thread = new Thread(new Runnable() {
+            public void run() {
+                while (!Thread.currentThread().isInterrupted() && !stopThread) {
+                    try {
+                        int byteCount = inputStream.available();
+                        if (byteCount > 0) {
+                            byte[] rawBytes = new byte[byteCount];
+                            inputStream.read(rawBytes);
+                            final String string = new String(rawBytes, "UTF-8");
+                            handler.post(new Runnable() {
+                                public void run() {
+                                    textView.setText(string);
+                                    ;
+                                }
+                            });
+
+                        }
+                    } catch (IOException ex) {
+                        stopThread = true;
+                    }
+                }
+            }
+        });
+
+        thread.start();
+    }
+
+
+    public void onClickConnect(View view) {
+        if (BTinit()) {
+            if (BTconnect()) {
+                deviceConnected = true;
+                connectButton.setEnabled(false);
+                beginListenForData();
+            }
         }
     }
 
