43b5972a48691554974f3ccaa00176ff584711f9
Daniel Pereira <danielpereira03@gmail.com>
2016-12-13 03:07:31 -0200

    added google fit api

--- app/src/main/java/com/example/dpereira14/scale/MainActivity.java
+++ app/src/main/java/com/example/dpereira14/scale/MainActivity.java
@@ -13,14 +13,28 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.Scopes;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.data.DataPoint;
+import com.google.android.gms.fitness.data.DataSet;
+import com.google.android.gms.fitness.data.DataSource;
+import com.google.android.gms.fitness.data.DataType;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
 
     private static final int RC_SIGN_IN = 1;
     private static final String TAG = "Scale";
+    private static boolean connected = false;
     private GoogleApiClient mGoogleApiClient;
     TextView textView;
+    private static int weight = 90;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +47,34 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
+                .requestScopes(new Scope(Scopes.FITNESS_ACTIVITY_READ_WRITE), new Scope(Scopes.FITNESS_LOCATION_READ))
                 .build();
 
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .enableAutoManage(this /* MainActivity */, this /* OnConnectionFailedListener */)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .addApi(Fitness.HISTORY_API)
+                .addScope(new Scope(Scopes.FITNESS_ACTIVITY_READ_WRITE))
+                .addConnectionCallbacks(
+                        new GoogleApiClient.ConnectionCallbacks() {
+                            @Override
+                            public void onConnected(Bundle bundle) {
+                                Log.i(TAG, "Connected To Google Client");
+                                connected = true;
+                            }
+
+                            @Override
+                            public void onConnectionSuspended(int i) {
+                                // If your connection to the sensor gets lost at some point,
+                                // you'll be able to determine the reason and react to it here.
+                                if (i == GoogleApiClient.ConnectionCallbacks.CAUSE_NETWORK_LOST) {
+                                    Log.i(TAG, "Connection lost.  Cause: Network Lost.");
+                                } else if (i == GoogleApiClient.ConnectionCallbacks.CAUSE_SERVICE_DISCONNECTED) {
+                                    Log.i(TAG, "Connection lost.  Reason: Service Disconnected");
+                                }
+                            }
+                        }
+                )
                 .build();
     }
 
@@ -52,7 +89,6 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
             case R.id.sign_in_button:
                 signIn();
                 break;
-            // ...
         }
     }
     
@@ -81,10 +117,49 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
 
     }
 
-
     private void signIn() {
         Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
         startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+
+
+    private DataSet createWeightDataForRequest(DataType dataType, int dataSourceType, Object values,
+                                         long startTime, long endTime, TimeUnit timeUnit) {
+        DataSource dataSource = new DataSource.Builder()
+                .setAppPackageName("MyPersonalScale")
+                .setDataType(dataType)
+                .setType(dataSourceType)
+                .build();
+
+        DataSet dataSet = DataSet.create(dataSource);
+        DataPoint dataPoint = dataSet.createDataPoint().setTimeInterval(startTime, endTime, timeUnit);
+
+        if (values instanceof Integer) {
+            dataPoint = dataPoint.setIntValues((Integer) values);
+        } else {
+            dataPoint = dataPoint.setFloatValues((Float) values);
+        }
+
+        dataSet.add(dataPoint);
+
+        return dataSet;
+    }
+
+    private DataSet insertWeight() {
+        Log.i(TAG, "Creating a new data insert request.");
+
+
+        // set insertion time
+        Calendar cal = Calendar.getInstance();
+        Date now = new Date();
+        cal.setTime(now);
+        long endTime = cal.getTimeInMillis();
+        long startTime = cal.getTimeInMillis();
+
+        // Create a data set
+        DataSet dataSet = createWeightDataForRequest(DataType.TYPE_WEIGHT, 0, weight, startTime, endTime, TimeUnit.MILLISECONDS);
+        return dataSet;
     }
 
 
